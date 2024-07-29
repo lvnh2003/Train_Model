effb00f8cf9467a30b2a3ca419ce2a814776470f
lvnh2003 <your-email@example.com>
2024-07-29 14:59:40 +0700

    Linear Regression

--- linear_regression/run.py
+++ linear_regression/run.py
@@ -18,25 +18,34 @@ plt.ylabel('giá')
 '''np.hstack() dùng để nối các mảng với nhau
     np.ones((N, 1)) là Tạo ra một mảng hai chiều có kích thước N×1, 
     trong đó tất cả các phần tử đều có giá trị là 1.
+    mảng được tạo ra trong np.ones là mảng 2 chiều và mỗi mảng con 1 phần tử, 
+    số 1 trong np.ones là số lượng phần tử trong mảng con
 '''
 x = np.hstack((np.ones((N, 1)), x))
-print(np.ones((N, 1)))
+
 w = np.array([0.,1.]).reshape(-1,1)
 
 numOfIteration = 100
+# mảng lưu cost sau mỗi lần gradient descent
 cost = np.zeros((numOfIteration,1))
 learning_rate = 0.00001
 for i in range(1, numOfIteration):
+    # tích vô hướng của w và x trừ y là residual đại diện cho phần dư, tức là sai số của model, y^ - y
+    # r.shape = (30,1)
     r = np.dot(x, w) - y
+    # tính cost, r được coi là phần dư tức là y^ - y
     cost[i] = 0.5*np.sum(r*r)
+    # np.sum(r) tính toán toàn bộ độ sai lệch tên các điểm dữ liệu
+    # ∂J/∂w[0] = ∑(r) = ∑(w0 + w1.xi -yi) vì f(w0,w1) = 1/2*(w0 + w1*xi - yi)^2 nên theo đạo hàm ta có
     w[0] -= learning_rate*np.sum(r)
-    # correct the shape dimension
     w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1].reshape(-1,1)))
-    # print(cost[i])
+    
+    print(cost[i])
 predict = np.dot(x, w)
+print(predict)
 plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
 plt.show()
-
+print(w)
 x1 = 50
 y1 = w[0] + w[1] * 50
 print('Giá nhà cho 50m^2 là : ', y1)
